742d454ad00d64eb33df849f858b2e82a68632d1
Waihon Yew <yewwaihon@gmail.com>
2019-02-23 14:18:45 +0800

    Database: Update User model to validate username and authenticate password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false }
+
+  def self.authenticate(username, password)
+    user = User.find_by(username: username)
+    user && user.authenticate(password)
+  end
 end
