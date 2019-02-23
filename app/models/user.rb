2dfcfc494cb50fdaab87b19afac8dd5093d3871c
Waihon Yew <yewwaihon@gmail.com>
2019-02-23 17:52:14 +0800

    Database: Add a default scope to TackleBoxItem, and an association to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false }
 
+  has_many :tackle_box_items, dependent: :destroy
+
   def self.authenticate(username, password)
     user = User.find_by(username: username)
     user && user.authenticate(password)
