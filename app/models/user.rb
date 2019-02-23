87af90b660889b0e86abd07273971d5b9e3a2247
Waihon Yew <yewwaihon@gmail.com>
2019-02-23 19:08:48 +0800

    Database: Update Catch with validations and a scope, and User with an association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
                        uniqueness: { case_sensitive: false }
 
   has_many :tackle_box_items, dependent: :destroy
+  has_many :catches, dependent: :destroy
 
   def self.authenticate(username, password)
     user = User.find_by(username: username)
